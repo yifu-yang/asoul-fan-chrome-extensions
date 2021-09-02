b02352e8882435c839df1ce517e418d3d7d38b9b
blackjack <576843117@qq.com>
2021-09-03 00:09:12 +0800

    finish asoul cnki

--- ASoulCnki/popup.js
+++ ASoulCnki/popup.js
@@ -6,7 +6,7 @@
 
 // Search the bookmarks when entering the search keyword.
 $('#search').on("click", function () {
-  console.log($('#asoultext').val());
+  //console.log($('#asoultext').val());
 
   $.ajax({
     method: "POST",
@@ -17,6 +17,21 @@ $('#search').on("click", function () {
     }),
     success: function (data) {
       console.log(data);
+      if (data.code != 0) {
+        $('#result').html("你的输入有误，请重试</br>");
+        return;
+      }
+      if (data.data.related.length == 0) { 
+        $('#result').html("恭喜你，你的是原创小作文，请继续保持！</br>"); 
+      }else{
+        let result = '';
+        result += "重复率：" + data.data.rate + "</br>";
+        let relatedText = data.data.related[0]
+        let source = relatedText.reply_url;
+        let ctime = new Date(parseInt(relatedText.reply.ctime) * 1000).toLocaleString().replace(/:\d{1,2}$/,' ');
+        $('#result').html(result+"相似评论："+source+"</br>"+"创建时间: "+ctime+"</br>");
+      }
+      $('#result').html("创建时间: "+ctime+"</br>");
     }
   });
 });
